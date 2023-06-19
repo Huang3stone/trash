89d384a5e74cabd0b04e25689f412235a32ad2cf
hl <614742675@qq.com>
2023-06-19 22:25:54 +0800

    delete all

--- SMNet-main/test.sh
+++
@@ -1,1 +0,0 @@
-python test.py  --test_folder  ./datasets/LOL/test/low  --output  ./output_test  --modelfile ./model_LOL.pth --modeltype LOL
--- SMNet-main/train.sh
+++
@@ -1,1 +0,0 @@
-python train.py --trainset  ./datasets/LOL/train  --testset  ./datasets/LOL/test  --output  ./output
